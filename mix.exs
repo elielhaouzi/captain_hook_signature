f46e16359b0f6d1cbea16349d599c8959329cb53
Eliel Haouzi <eliel.haouzi@gmail.com>
2025-03-11 11:33:37 +0200

    add conn to the verify function for modules who needs the request url as part of the verify signature process. (#1)

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule CaptainHookSignature.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/elielhaouzi/captain_hook_signature"
-  @version "0.6.1"
+  @version "0.6.2"
 
   def project do
     [
